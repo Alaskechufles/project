cbd140a213805db639442795c0806f2554e4c716
Diego <alask27echufles@gmail.com>
2023-06-18 16:07:10 -0500

    ya casi solo falta ajustar el filtro

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,8 @@ function App() {
       const resJson = await res.json();
       // Aquí guardamos los datos de "stays.json" en la variable data.
       setData(resJson);
+      // Aquí gurdo los datos tambien en el contenedor
+      setContenedor(resJson);
     } catch (error) {
       console.log(error);
     }
@@ -31,7 +33,7 @@ function App() {
   }, []);
 
   // Puedes ver la variable data en consola.
-  /* console.log(data); */
+  console.log(data);
   //filtro de busqueda
 
   //conteo de personas
@@ -49,16 +51,17 @@ function App() {
   const buttonRedC = () => {
     setCountC(countC - 1);
   };
-  let numG = countA + countC
-  let numGuest = numG ? `${numG} guests` : "Add guests";
+  /* let numG = parseInt(countA + countC) */
+  /* let numGuest = numG ? `${numG} guests` : "Add guests"; */
   //cambio de location
-  const input = document.getElementById("locationInput")
+  //! no permite buscar correctamente por eso esta inhabilitado
+  /* const input = document.getElementById("locationInput")
   const [location, setLocation] = useState("")
 
   function insertLocation(parameter, el = input) {
     setLocation(parameter)
     el.value = location
-  }
+  } */
 
 
   //mostrar y ocultar opciones
@@ -70,51 +73,62 @@ function App() {
   const claseElemento = isActive ? "bar active" : "bar search"
 
   const backg = isActive ? "b noBack" : "b back"
+
+  /*
+  * aqui podemo agregar mostrar las opciones de busqueda
+   */
+
   //filtro
-  /* const [filtro, setFiltro] = useState('');
-  const [elementosFiltrados, setElementosFiltrados] = useState(data);
+  /* const [data, setData] = useState([]) 
+  *Este esta declarado en la parte de arriba linea 13
+  */
+  const [contenedor, setContenedor] = useState([])
+  const [filtro, setFiltro] = useState("")
+  const [filtroCamas, setFiltroCamas] = useState("")
 
 
 
-  const aplicarFiltro = () => {
-    const elementos = data.filter((elemento) => {
-      if (elemento.city && filtro) {
-        return elemento.city.toLowerCase().includes(filtro.toLowerCase());
+  //capturar lo que escribe el ususario en el input
+  const busqueda1 = (e) => {
+    setFiltro(e.target.value)
+
+
+    filtrado1(e.target.value)
+    /* console.log(e.target.value) */
+  }
+  const busqueda2 = (e) => {
+
+    setFiltroCamas(e.target.value)
+    filtrado2(e.target.value)
+    console.log(e.target.value)
+  }
+
+
+  /* const valL = document.getElementById("locationInput")
+  const valC = document.getElementById("camas") */
+
+
+  const filtrado1 = (texto) => {
+    const elementosFiltrados = contenedor.filter((elemento) => {
+      if (elemento.city.toString().toLowerCase().includes(texto.toLowerCase())
+      ) {
+        return elemento;
       }
-      return false;
+
     });
-    setElementosFiltrados(elementos);
-    console.log(elementosFiltrados)
-  };
-  const handleChangeFiltro = (event) => {
-    setFiltro(event.target.value);
-  };
+    setData(elementosFiltrados)
+  }
+  const filtrado2 = (camas) => {
+    let elementosFiltrados = contenedor.filter((elemento2) => {
+      if (
 
-  
-
-  useEffect(() => {
-    setElementosFiltrados(data);
-  }, []); */
-  const [filtro, setFiltro] = useState('');
-  const [elementosFiltrados, setElementosFiltrados] = useState(data);
-
-  const aplicarFiltro = () => {
-    const elementos = data.filter((elemento) => {
-      if (elemento.city && filtro) {
-        return elemento.city.toLowerCase().includes(filtro.toLowerCase());
+        elemento2.maxGuests == camas
+      ) {
+        return elemento2;
       }
-      return false;
     });
-    setElementosFiltrados(elementos);
-  };
-
-  const handleChangeFiltro = (event) => {
-    setFiltro(event.target.value);
-  };
-
-  useEffect(() => {
-    setElementosFiltrados(data);
-  }, []);
+    setData(elementosFiltrados)
+  }
 
   return (
     <Fragment>
@@ -126,39 +140,39 @@ function App() {
             <div className="location-area">
               <div className="location-head">
                 <h2>LOCATION</h2>
-                <input id="locationInput" type="text" placeholder="Add location" value={filtro} onChange={handleChangeFiltro} />
+                <input id="locationInput" type="text" placeholder="Add location" value={filtro} onChange={busqueda1} />
               </div>
               <div className="location-options">
                 <div className="option">
                   <div className="box-icon-search">
                     <Geo />
                   </div>
-                  <h2 onClick={() => insertLocation("Helsinki")}>Helsinki, Finland</h2>
+                  <h2 >Helsinki, Finland</h2>
                 </div>
                 <div className="option">
                   <div className="box-icon-search">
                     <Geo />
                   </div>
-                  <h2 onClick={() => insertLocation("Turku")}>Turku, Finland</h2>
+                  <h2>Turku, Finland</h2>
                 </div>
                 <div className="option">
                   <div className="box-icon-search">
                     <Geo />
                   </div>
-                  <h2 onClick={() => insertLocation("Oulu")}>Oulu, Finland</h2>
+                  <h2 >Oulu, Finland</h2>
                 </div>
                 <div className="option">
                   <div className="box-icon-search">
                     <Geo />
                   </div>
-                  <h2 onClick={() => insertLocation("Vaasa")}>Vaasa, Finland</h2>
+                  <h2 >Vaasa, Finland</h2>
                 </div>
               </div>
             </div>
             <div className="guests-area">
               <div className="guests-head">
                 <h2>Guests</h2>
-                <input type="text" placeholder="Add guests" defaultValue={numGuest} />
+                <input id="camas" type="text" placeholder="Add guests" value={filtroCamas} onChange={busqueda2} />
               </div>
               <div className="guests-options">
                 <div className="adults">
@@ -183,10 +197,7 @@ function App() {
             </div>
             <div className="search-area">
               <div className="search-button">
-                <button className="search-button-cont" onClick={() => {
-                  aplicarFiltro();
-                  toggleClass();
-                }} >
+                <button className="search-button-cont" onClick={() => { toggleClass(); /* filtrado1(valL, valC) *//* ; filtrado2() */ }} >
                   <div className="lupa-search">
                     <SearchIcon />
                   </div>
@@ -216,7 +227,7 @@ function App() {
       </div>
       <div className="tarjetas">
         {/* Aquí te dejo un ejemplo de cómo podrías imprimir varios elementos a la vez. */}
-        {elementosFiltrados.map((el, i) => {
+        {data && data.map((el, i) => {
           return (
             <div className="tarjeta" key={i}>
               <Tarjeta imgs={el.photo} content={el.type} rate={el.rating} descript={el.title} super={el.superHost} bed={el.beds} />
